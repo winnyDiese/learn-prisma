566e9ae40121ed7db3810f4552796fd7294b0fd8
winnyDiese <winnydiese@gmail.com>
2022-09-10 23:50:00 +0200

    Show the user with the relation table

--- script.ts
+++ script.ts
@@ -6,29 +6,52 @@ import { disconnect } from "process"
 const prisma =  new PrismaClient()
 
 
+// // show the user without the relation table (posts)
+// async function main(){
+//     const user = await prisma.user.create({
+//         data: {
+//             name: 'Bob',
+//             email: 'bob@prisma.io',
+//             posts:{
+//                 create:{
+//                     title: 'Hello world'
+//                 }
+//             }
+//         }
+//     })
+ 
+//     console.log(user)
+// }
+
+
+// main()
+// .then(async () =>{
+//     await prisma.$disconnect()
+// })
+// .catch(async e =>{
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+// })
+
+
+
+// Show the user with the relation table
 async function main(){
-    const user = await prisma.user.create({
-        data: {
-            name: 'Bob',
-            email: 'bob@prisma.io',
-            posts:{
-                create:{
-                    title: 'Hello world'
-                }
-            }
+    const userWithPosts = await prisma.user.findMany({
+        include:{
+            posts: true
         }
     })
- 
-    console.log(user)
+    console.dir(userWithPosts, {depth: null})
 }
-
 
 main()
 .then(async () =>{
     await prisma.$disconnect()
 })
 .catch(async e =>{
-    console.error(e)
+    console.log(e)
     await prisma.$disconnect()
     process.exit(1)
 })
