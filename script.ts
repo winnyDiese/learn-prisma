2d4fd1f731fe377bbdbb1cc03cf1cb14a796bcf7
winnyDiese <winnydiese@gmail.com>
2022-09-10 12:42:46 +0200

    Retrieve or to Get all (user) data from Bdd

--- script.ts
+++ script.ts
@@ -6,27 +6,24 @@ import { disconnect } from "process"
 const prisma =  new PrismaClient()
 
 
-// Creation of one enregistrement a new user
-// for to add it in a Bdd
-async function main() {
-    const user = await prisma.user.create({
-        data: {
-            name: 'Alice',
-            email: 'alice@prisma.io'
-        }
-    })
-
-    console.log(user)
+// Retrieve all (user) data 
+async function main(){
+    const users = await prisma.user.findMany()
+    console.log(users)
 }
 
 main()
 .then(async ()=>{
     await prisma.$disconnect()
 })
-.catch(async (e)=>{
+.catch(async e =>{
     console.error(e)
     await prisma.$disconnect()
     process.exit(1)
 })
+
+
+
+
 
 
